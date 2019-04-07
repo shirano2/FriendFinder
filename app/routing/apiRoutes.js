56bb1748f0e1daa39f5db7bf6e4944925117a26b
shirano2 <shirano2@naver.com>
2019-04-07 12:36:44 -0500

    finished

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,2 +1,32 @@
-// /api/friends  --> get JSON
-// /api/friends --> getting data of survery
+const express=require("express");
+const router = express.Router();
+const friendArray=require("../data/friends.js");
+
+router.use(function timeLog(req, res,next) {
+    next(); 
+});
+
+router.get("/",function(req,res){
+    res.json(friendArray);
+});
+
+router.post("/",function(req,res){
+    var newFriend=req.body;
+    var matchFriend;
+    var match;
+    var score=0;
+    for(var i=0;i<friendArray.length;i++) {
+        for(var j=0;j<friendArray[i].scores.length;j++) {
+            score=score+Math.abs(friendArray[i].scores[j]-newFriend.scores[j]);
+        }
+        if(match==undefined || score<match) {
+            match=score;
+            matchFriend=friendArray[i];
+        }
+        score=0;
+    }
+    friendArray.push(newFriend);
+    res.json(matchFriend);
+});
+
+module.exports=router;
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,2 +1,13 @@
-// /servey
-// survey.html
+const express=require("express");
+const router = express.Router();
+const path=require("path");
+
+router.use(function timeLog(req, res,next) {
+    next(); 
+});
+
+router.get("/",function(req,res){
+    res.sendFile(path.join(__dirname,"../public/survey.html"));
+});
+
+module.exports=router;
